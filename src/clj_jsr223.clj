8da5e7973726febda76d610357a8b06ff7ad4637
Eduardo Díaz <iamedu@gmail.com>
2013-11-16 13:52:23 -0600

    Takes *out* and *err* into account

--- src/clj_jsr223.clj
+++ src/clj_jsr223.clj
@@ -12,7 +12,7 @@
 
 (ns ^{:author "Eduardo Julián <eduardoejp@gmail.com>",
       :doc "Clojure implementations of the ScriptEngine and ScriptEngineFactory interfaces."}
-     clj-jsr223
+  clj-jsr223
   (:import (javax.script Bindings ScriptContext)))
 
 (deftype ClojureScriptEngine [factory context]
@@ -24,27 +24,28 @@
   (getBindings [_ ctx] (.getBindings @context ctx))
   (setBindings [_ binds ctx] (.setBindings @context binds ctx))
   (put [self k v] (.put (.getBindings self) k v))
-  (eval ^Object [self ^String script] (eval (read-string script)))
+  (eval ^Object [self ^String script] (.eval self (read-string script) (.getBindings (.getContext self))))
   (eval ^Object [self ^String script ^Bindings binds]
     (doseq [[k v] (->> binds seq (apply conj {}))]
       (let [parts (.split k "/")
             [ns name] (if (= 2 (count parts)) (map symbol parts) ['user (symbol (first parts))])]
         (create-ns ns)
         (intern ns name v)))
-    (.eval self script))
+    (binding [*out* (-> (.getContext self) (.getWriter))
+              *err* (-> (.getContext self) (.getErrorWriter))]
+      (.eval self script)))
   (eval ^Object [self ^String script ^ScriptContext ctx] (.eval self script (.getBindings ctx)))
   (eval ^Object [self ^java.io.Reader script] (.eval self (slurp script)))
   (eval ^Object [self ^java.io.Reader script ^Bindings binds] (.eval self (slurp script) binds))
-  (eval ^Object [self ^java.io.Reader script ^ScriptContext ctx] (.eval self (slurp script) (.getBindings ctx)))
-  )
+  (eval ^Object [self ^java.io.Reader script ^ScriptContext ctx] (.eval self (slurp script) (.getBindings ctx))))
 
 (deftype ClojureScriptEngineFactory []
   javax.script.ScriptEngineFactory
   (getParameter [_ _] nil)
   (getEngineName [_] "Clojure Scripting Engine")
-  (getEngineVersion [_] "1.4")
+  (getEngineVersion [_] "1.5.1")
   (getLanguageName [_] "Clojure")
-  (getLanguageVersion [_] "1.4")
+  (getLanguageVersion [_] "1.5.1")
   (getExtensions [_] ["clj"])
   (getMimeTypes [_] ["application/clojure" "text/clojure"])
   (getNames [_] ["clojure" "Clojure"])
